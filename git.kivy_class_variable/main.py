02ec47ab7c559dc832c5dae43a3835b10b01b442
orderbyunion <66033605+orderbyunion@users.noreply.github.com>
2022-04-24 01:52:32 +0100

    Add files via upload

---
+++ git.kivy_class_variable/main.py
@@ -0,0 +1,80 @@
+import socket 
+import gi
+gi.require_version('Gst', '1.0')
+import kivy
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget 
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty, ReferenceListProperty
+from kivy.graphics.texture import Texture
+from kivy.core.camera import Camera
+from kivy.graphics import *
+import time
+import os 
+from pathlib import Path 
+#import cv2											
+import struct
+import threading
+import pickle
+
+Builder.load_file('the.kv')
+      
+
+class fscreen(Widget):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+	
+	def change(self):
+		theapp.screenm.current = 'second screen'
+
+		
+	def change_color(self):
+		theapp.secscreen.ids.lb.color = [1, 0, 0,1]
+		theapp.secscreen.ids.lb.text = 'color has been changed'
+
+
+
+
+class secscreen(Widget):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+	
+	def change(self):
+		theapp.screenm.current = 'first screen'	
+
+	def change_color(self):
+		theapp.fscreen.ids.lb.color = [1, 0, 0,1]
+		theapp.fscreen.ids.lb.text = 'color has been changed'
+
+
+class theapp(App):
+	def build(self):
+		
+		self.screenm = ScreenManager() 
+		self.fscreen = fscreen()
+		screen = Screen(name = "first screen")
+		screen.add_widget(self.fscreen)
+		self.screenm.add_widget(screen)
+
+		self.secscreen = secscreen()
+		screen = Screen(name = "second screen")
+		screen.add_widget(self.secscreen)
+		self.screenm.add_widget(screen)
+
+		return self.screenm
+
+if __name__ == "__main__":
+	theapp = theapp()										
+	theapp.run() 
