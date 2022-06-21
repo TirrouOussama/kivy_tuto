c5f44e536482c99f59b829595db3dc1d35fe818a
Tirrou Oussama <66033605+TirrouOussama@users.noreply.github.com>
2022-06-21 14:41:08 +0100

    Update main.py

--- git.kivy_class_variable/main.py
+++ git.kivy_class_variable/main.py
@@ -7,7 +7,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget 
 from kivy.uix.textinput import TextInput
